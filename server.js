1f5bb8f79a049509eceaa5ea29e8360b0f293076
Maxim Ryabtsev <99872972+lipophrenia@users.noreply.github.com>
2023-04-25 16:19:00 +0300

    first

---
+++ server.js
@@ -0,0 +1,46 @@
+const host = 'localhost';
+const port = 3030;
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser')
+
+const express = require("express");
+const app = express();
+ 
+app.use(express.json());
+app.use(bodyParser.json());
+
+// app.use(function (request, response) {
+//   response.sendFile(__dirname + "/www/index.html");
+// });
+
+app.get('/index', function (req, res) {
+	res.sendFile(path.join(__dirname, 'www', 'index.html'));
+});
+
+app.patch("/api/update", function (request, response) {
+
+  var fileName = '/home/practice/test/test.json';
+  var data = JSON.stringify(request.body);
+
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      response.status(500).json({ "msg": "Update error" });
+      console.error(err);
+    }
+    else {
+      response.status(200).json({ "msg": "File updated" });
+    }
+  });
+
+  
+  //console.log(request.body);
+  if(!request.body) return response.sendStatus(400);
+  
+  response.json(request.body);
+});
+  
+
+app.listen((port),()=>{
+  console.log(`Server is running on http://${host}:${port}/index`);
+});
